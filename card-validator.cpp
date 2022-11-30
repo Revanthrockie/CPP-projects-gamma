e5908ce68de44f7b1e8d75760bc1f4a2b5fc115c
Revanth Rockie <rockierevanth@gmail.com>
2022-12-01 01:11:38 +0530

    done with writing code. Still got to debug the code :)

--- card-validator.cpp
+++ card-validator.cpp
@@ -1,1 +1,85 @@
+<<<<<<< current
 // master card number
+=======
+// master card number - 5
+// visa card number - 4
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+  long check_card;
+  int sum = 0 , count = 0;
+  char result[] = 18;
+
+  do{
+    cout << "Please enter your card number: \n";
+    cin >> check_card;
+  }while ( check_card <= 0);
+
+  // step 1:
+
+  long work_card = check_card;
+  int o_sum = 0 , divisor = 10;;
+
+  while (work_card > 0)
+  {
+    int LastDigit = work_card % 10;
+    o_sum += LastDigit;
+    sum = sum + o_sum;
+
+    work_card = work_card/100;
+
+  }
+
+  // step 2:
+
+  while (work_card > 0)
+  {
+    int LasteDigit = (work_card / 10) % 10;
+    int timestwo = LasteDigit * 2;
+    int e_sum = (LasteDigit / 10) + (LasteDigit % 10);
+    sum = sum + e_sum;
+
+    }
+
+    work_card = check_card;
+    while (work_card != 0)
+    {
+      work_card = work_card / 10;
+      count++;
+
+    }
+
+  // Code to check what kind of card it is (1)
+ for (int i = 0; i < count - 2; i++)
+ {
+   divisor = divisor*10;
+ }
+
+ // To check what kind of card it is (2)
+
+ int FirstDigitnum = check_card / divisor;
+ int SecondDigitnum = check_card / ( divisor / 10);
+
+ if( sum % 10 == 0 ){
+   if( FirstDigitnum == 4 && (count == 13 || count == 16))
+   {
+     strcpy(result , "VISA");
+   }else if( (SecondDigitnum == 34 || SecondDigitnum == 37) && count == 15)
+    {
+     strcpy(result , "A.EXP");
+   }else if( (50 < SecondDigitnum || SecondDigitnum < 56 )  || count == 15 )
+   {
+     strcpy(result , "MASTERCARD");
+   }else
+   {
+     strcpy(result , "INVALID CARD");
+
+   }
+ }
+
+};
+>>>>>>> before discard
